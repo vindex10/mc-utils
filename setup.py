5a453e530e5687cc667932dae3deda9a3ff928b2
Victor Ananyev <vindex10@gmail.com>
2020-06-22 16:58:00 +0200

    helmg -> mc utils

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name="helmg-utils",
+setup(name="mc-utils",
       version="9999",
       description="Helper functions to process events",
-      url="https://github.com/vindex10/hyy4e-helmg",
+      url="https://github.com/vindex10/mc-utils",
       author="Victor Ananyev",
       author_email="vindex10@gmail.com",
-      packages=["helmg_utils"],
+      packages=["mc_utils"],
       install_requires=["numpy==1.16.*"])
